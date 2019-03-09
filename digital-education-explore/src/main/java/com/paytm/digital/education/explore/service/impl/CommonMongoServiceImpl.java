415fc69b78da941d654acdd5f5b946d822df48be
Yasser Hussain <yasser.hussain@paytmmall.com>
2019-03-09 13:54:25 +0000

    Merged in EDU-88 (pull request #40)
    EDU-88 added spring caching for field groups
    
    Approved-by: Girish Patel <girish.patel@paytmmall.com>

--- digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/impl/CommonMongoServiceImpl.java
+++ digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/impl/CommonMongoServiceImpl.java
@@ -3,6 +3,7 @@ package com.paytm.digital.education.explore.service.impl;
 import com.paytm.digital.education.explore.database.repository.CommonMongoRepository;
 import com.paytm.digital.education.explore.service.CommonMongoService;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class CommonMongoServiceImpl implements CommonMongoService {
     private CommonMongoRepository commonMongoRepository;
 
     @Override
+    @Cacheable(value = "fieldGroups")
     public List<String> getFieldsByGroupAndCollectioName(String collectionName, String fieldGroup) {
         return commonMongoRepository.getFieldsByGroupAndCollectioName(collectionName, fieldGroup);
     }
