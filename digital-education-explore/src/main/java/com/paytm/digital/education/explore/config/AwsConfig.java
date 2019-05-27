b0250963aafc5a73a31d94dcaa4238307ecd5b42
Gaurav Kumar Das <gaurav.das@paytm.com>
2019-05-27 15:08:46 +0530

    EDU-1282 : Aws config function

---
+++ digital-education-explore/src/main/java/com/paytm/digital/education/explore/config/AwsConfig.java
@@ -0,0 +1,37 @@
+package com.paytm.digital.education.explore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class AwsConfig {
+
+    @Bean
+    public static AmazonS3 func() {
+        String clientRegion = "ap-southeast-1";
+
+        try {
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(clientRegion)
+                    .withCredentials(new ProfileCredentialsProvider())
+                    .build();
+            return s3Client;
+        } catch (AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process
+            // it, so it returned an error response.
+            log.error("Error In AWS.func() with AmazonServiceException ", e);
+        } catch (SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            log.error("Error In AWS.func() with SdkClientException ", e);
+        }
+        return null;
+    }
+}
