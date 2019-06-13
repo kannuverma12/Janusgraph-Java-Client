39f3ecc03444b5f97268fb652e71e61a1f726494
Lakshay Badak <lakshay.badak@paytm.com>
2019-06-13 11:01:06 +0000

    Merged in EDU-1611 (pull request #397) * EDU-1611:Monstache index update
    Approved-by: Karan Verma <karan2.verma@paytmmall.com>

--- monstache/transformLocation.js
+++ monstache/transformLocation.js
@@ -1,12 +1,11 @@
 module.exports = function(doc) {
-	var targetLocation = {};
-	targetLocation.location_id = doc.location_id;
-	targetLocation.name = [];
-	name.push(doc.loc_string);
-	targetLocation.official_name = doc.loc_string;
-	targetLocation.state_id = doc.state_id;
-	targetLocation.city_id = doc.city_id;
-	targetLocation.entity_type = 'location';
-	console.log ("doc: " + targetLocation);
-	return targetLocation;
+        var targetLocation = {};
+        targetLocation.location_id = doc.location_id;
+        targetLocation.names = [doc.loc_string];
+        targetLocation.official_name = doc.loc_string;
+        targetLocation.state_id = doc.state_id;
+        targetLocation.city_id = doc.city_id;
+        targetLocation.entity_type = 'location';
+        console.log ("doc: " + targetLocation);
+        return targetLocation;
 }
