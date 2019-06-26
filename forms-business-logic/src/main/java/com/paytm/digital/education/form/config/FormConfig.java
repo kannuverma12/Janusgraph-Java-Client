3569b748e0e20c3a96c9ef8f470e30dcc31b91c5
Yasser Hussain <yasser.hussain@paytm.com>
2019-06-26 09:55:26 +0000

    Merged in EDU-1774-cors-issue-fix-for-fbl-explore (pull request #422)
    EDU-1774 Added cors fix code
    
    Approved-by: Yasser Hussain <yasser.hussain@paytm.com>
    Approved-by: Girish Patel <girish.patel@paytmmall.com>

--- forms-business-logic/src/main/java/com/paytm/digital/education/form/config/FormConfig.java
+++ forms-business-logic/src/main/java/com/paytm/digital/education/form/config/FormConfig.java
@@ -1,8 +1,6 @@
 package com.paytm.digital.education.form.config;
 
-import lombok.AllArgsConstructor;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -12,23 +10,20 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import paytm.auth.personaaclclient.infrastructure.security.CookieAuthenticationProvider;
-import paytm.auth.personaaclclient.infrastructure.security.DomainUsernamePasswordAuthenticationProvider;
 
-//@EnableWebSecurity
-@AllArgsConstructor
-//@Order(2)
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class FormConfig extends WebSecurityConfigurerAdapter {
-    private LCPTokenAuthenticationProvider tokenAuthenticationProvider;
-
-    private DomainUsernamePasswordAuthenticationProvider domainUsernamePasswordAuthenticationProvider;
-
-    private CookieAuthenticationProvider cookieAuthenticationProvider;
+    private final LCPTokenAuthenticationProvider tokenAuthenticationProvider;
+    private final CookieAuthenticationProvider cookieAuthenticationProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/formfbl/v1/orders").authenticated()
                 .antMatchers("/formfbl/v1/orders/download").authenticated()
                 .antMatchers("/formfbl/v1/orders/bulk-download").authenticated()
@@ -41,7 +36,7 @@ public class FormConfig extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(new FormAuthenticationFilter(authenticationManager()),
                 BasicAuthenticationFilter.class);
     }
-    
+
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/explore");
