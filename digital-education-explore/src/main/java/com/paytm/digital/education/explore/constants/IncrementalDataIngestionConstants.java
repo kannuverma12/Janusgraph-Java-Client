8ac56cae4725d9bef598e68963e9f4edd2b9aea8
Gaurav Kumar Das <gaurav.das@paytm.com>
2019-07-09 19:00:24 +0530

    EDU-2003 : File version is retrieved from database

--- digital-education-explore/src/main/java/com/paytm/digital/education/explore/constants/IncrementalDataIngestionConstants.java
+++ digital-education-explore/src/main/java/com/paytm/digital/education/explore/constants/IncrementalDataIngestionConstants.java
@@ -1,10 +1,15 @@
 package com.paytm.digital.education.explore.constants;
 
 public interface IncrementalDataIngestionConstants {
-    String EXAM_FILE_NAME = "exams.json";
-    String INSTITUTE_FILE_NAME = "institutions.json";
-    String COURSES_FILE_NAME = "courses.json";
+    String EXAM_FILE_NAME                  = "exams.json";
+    String INSTITUTE_FILE_NAME             = "institutions.json";
+    String COURSES_FILE_NAME               = "courses.json";
     String SFTP_INSTITUTE_FILE_NAME_FORMAT = "institutions_{0}.json";
-    String SFTP_COURSE_FILE_NAME_FORMAT = "courses_{0}.json";
-    String SFTP_EXAM_FILE_NAME_FORMAT = "exams_{0}.json";
+    String SFTP_COURSE_FILE_NAME_FORMAT    = "courses_{0}.json";
+    String SFTP_EXAM_FILE_NAME_FORMAT      = "exams_{0}.json";
+    String NEXT_INSTITUTE_FILE_VERSION     = "next_institute_file_version";
+    String NEXT_COURSE_FILE_VERSION        = "next_course_file_version";
+    String NEXT_EXAM_FILE_VERSION          = "next_exam_file_version";
+    String DATA_INGESTION                  = "data_ingestion";
+    String INCREMENTAL                     = "incremental";
 }
--- digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/helper/IncrementalDataHelper.java
+++ digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/helper/IncrementalDataHelper.java
@@ -4,6 +4,7 @@ import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.Session;
 import com.paytm.digital.education.dto.SftpConfig;
 import com.paytm.digital.education.explore.config.DataIngestionSftpConfig;
+import com.paytm.digital.education.property.reader.PropertyReader;
 import com.paytm.digital.education.service.SftpService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +16,15 @@ import java.util.Map;
 import java.util.Vector;
 
 import static com.paytm.digital.education.constant.SftpConstants.CHANNEL_TYPE;
+import static com.paytm.digital.education.explore.constants.ExploreConstants.EXPLORE_COMPONENT;
 import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.COURSES_FILE_NAME;
+import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.DATA_INGESTION;
 import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.EXAM_FILE_NAME;
+import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.INCREMENTAL;
 import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.INSTITUTE_FILE_NAME;
+import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.NEXT_COURSE_FILE_VERSION;
+import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.NEXT_EXAM_FILE_VERSION;
+import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.NEXT_INSTITUTE_FILE_VERSION;
 import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.SFTP_COURSE_FILE_NAME_FORMAT;
 import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.SFTP_EXAM_FILE_NAME_FORMAT;
 import static com.paytm.digital.education.explore.constants.IncrementalDataIngestionConstants.SFTP_INSTITUTE_FILE_NAME_FORMAT;
@@ -26,7 +33,8 @@ import static com.paytm.digital.education.explore.constants.IncrementalDataInges
 @Service
 @AllArgsConstructor
 public class IncrementalDataHelper {
-    private SftpService sftpService;
+    private SftpService    sftpService;
+    private PropertyReader propertyReader;
 
     public Map<String, Boolean> downloadFileFromSftp() {
         SftpConfig sftpConfig = new SftpConfig();
@@ -40,9 +48,15 @@ public class IncrementalDataHelper {
         fileExistFlags.put(EXAM_FILE_NAME, false);
         fileExistFlags.put(INSTITUTE_FILE_NAME, false);
         fileExistFlags.put(COURSES_FILE_NAME, false);
-        String currentExamFileName = MessageFormat.format(SFTP_EXAM_FILE_NAME_FORMAT, 1);
-        String currentInstituteFileName = MessageFormat.format(SFTP_INSTITUTE_FILE_NAME_FORMAT, 1);
-        String currentCourseFileName = MessageFormat.format(SFTP_COURSE_FILE_NAME_FORMAT, 1);
+        Map<String, Object> versioningData = propertyReader
+                .getPropertiesAsMapByKey(EXPLORE_COMPONENT, DATA_INGESTION, INCREMENTAL);
+
+        String currentExamFileName = MessageFormat.format(SFTP_EXAM_FILE_NAME_FORMAT,
+                versioningData.get(NEXT_EXAM_FILE_VERSION));
+        String currentInstituteFileName = MessageFormat.format(SFTP_INSTITUTE_FILE_NAME_FORMAT,
+                versioningData.get(NEXT_INSTITUTE_FILE_VERSION));
+        String currentCourseFileName = MessageFormat.format(SFTP_COURSE_FILE_NAME_FORMAT,
+                versioningData.get(NEXT_COURSE_FILE_VERSION));
         try {
             session = sftpService.createSession(sftpConfig);
             sftp = (ChannelSftp) session.openChannel(CHANNEL_TYPE);
