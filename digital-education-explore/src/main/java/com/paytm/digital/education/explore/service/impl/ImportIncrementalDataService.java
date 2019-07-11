597afcfb8c5847ed2b458f0ed1d4109dd306c032
Gaurav Kumar Das <gaurav.das@paytm.com>
2019-07-11 18:20:43 +0530

    EDU-2003 : EDU-2008 is merged

--- digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/impl/ImportIncrementalDataService.java
+++ digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/impl/ImportIncrementalDataService.java
@@ -1,7 +1,9 @@
 package com.paytm.digital.education.explore.service.impl;
 
+import com.paytm.digital.education.explore.database.entity.Institute;
 import com.paytm.digital.education.explore.database.ingestion.Course;
 import com.paytm.digital.education.explore.database.ingestion.Exam;
+import com.paytm.digital.education.explore.dto.InstituteDto;
 import com.paytm.digital.education.explore.service.helper.IncrementalDataHelper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,11 +25,16 @@ public class ImportIncrementalDataService {
     private IncrementalDataHelper         incrementalDataHelper;
     private TransformAndSaveCourseService transformAndSaveCourseService;
     private TransformAndSaveExamService   transformAndSaveExamService;
+    private TransformInstituteService     transformInstituteService;
 
     public boolean importData() throws FileNotFoundException {
         Map<String, Boolean> fileInfo = incrementalDataHelper.downloadFileFromSftp();
         if (fileInfo.get(INSTITUTE_FILE_NAME)) {
-            // Insert institute data
+            List<InstituteDto> instituteDtos =
+                    incrementalDataHelper.retrieveDataFromFile(INSTITUTE_FILE_NAME,
+                            InstituteDto.class);
+            List<Institute> institutes =
+                    transformInstituteService.transformInstituteDtos(instituteDtos);
         }
         if (fileInfo.get(EXAM_FILE_NAME)) {
             List<Exam> examDtos = incrementalDataHelper.retrieveDataFromFile(EXAM_FILE_NAME,
