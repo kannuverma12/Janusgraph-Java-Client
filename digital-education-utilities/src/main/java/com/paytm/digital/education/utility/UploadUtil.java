06b62e5b610ae1d34184927dce5f5f3c19d040e1
kverma07 <karan2.verma@paytm.com>
2019-08-13 18:23:36 +0530

    EDU-2534 sftp production readiness

--- digital-education-utilities/src/main/java/com/paytm/digital/education/utility/UploadUtil.java
+++ digital-education-utilities/src/main/java/com/paytm/digital/education/utility/UploadUtil.java
@@ -57,7 +57,9 @@ public class UploadUtil {
 
     public String uploadImage(String fileUrl, String fileName, Long instituteId,
             String s3BucketName, String s3ImagePath) {
-        System.setProperty("http.agent", "Chrome");
+        System.setProperty("http.agent",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                        + "Chrome/28.0.1500.29 Safari/537.36");
         try {
             URL url = new URL(fileUrl);
             InputStream stream = url.openStream();
