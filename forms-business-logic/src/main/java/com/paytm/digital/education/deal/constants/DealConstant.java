e0a61ed0ee23ae585381823e523379157b9472a3
Girish Patel <girish.patel@paytmmall.com>
2019-08-20 16:38:48 +0530

    EDU-2395 Removed FBL module

--- forms-business-logic/src/main/java/com/paytm/digital/education/deal/constants/DealConstant.java
+++
@@ -1,8 +0,0 @@
-package com.paytm.digital.education.deal.constants;
-
-public interface DealConstant {
-    String YYYY_MM_DD                   = "yyyy-MM-dd";
-    String YYYY_MM_DD_T_HH_MM_SS        = "yyyy-MM-dd'T'HH:mm:ss";
-    String CUSTOMER_ID                  = "customer_id";
-    String REF_ID                       = "_id";
-}
