79e2ce65597dc315bd8076d3d858d51f85755675
Lakshay Badak <lakshay.badak@paytm.com>
2019-08-26 08:24:48 +0000

    Merged in hot-fix1 (pull request #577)
    Made headers optional
    
    Approved-by: Manoj Kumar <manoj27.kumar@paytm.com>

--- digital-education-explore/src/main/java/com/paytm/digital/education/explore/controller/LeadController.java
+++ digital-education-explore/src/main/java/com/paytm/digital/education/explore/controller/LeadController.java
@@ -50,9 +50,9 @@ public class LeadController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/auth/v1/user_details")
     public UserDetails getLeadUserDetails(@RequestHeader("x-user-id") @NotNull @Min(1) Long userId,
-            @RequestHeader("x-user-email") String email,
-            @RequestHeader("x-user-firstname") String firstName,
-            @RequestHeader("x-user-phone") @NotBlank String phone) {
+            @RequestHeader(value = "x-user-email", required = false) String email,
+            @RequestHeader(value = "x-user-firstname", required = false) String firstName ,
+            @RequestHeader(value = "x-user-phone", required = false) String phone) {
         return leadService.getUserDetails(userId, email, firstName, phone);
     }
 
