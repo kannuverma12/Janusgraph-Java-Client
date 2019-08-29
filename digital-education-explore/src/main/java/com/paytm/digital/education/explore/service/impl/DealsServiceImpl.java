227e1c4de70d2e8911684e05e3ba065f411f5561
kverma07 <karan2.verma@paytm.com>
2019-08-29 16:55:09 +0530

    EDU-StreamAPI Added deals api for stream list and year list

--- digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/impl/DealsServiceImpl.java
+++ digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/impl/DealsServiceImpl.java
@@ -2,17 +2,21 @@ package com.paytm.digital.education.explore.service.impl;
 
 import com.paytm.digital.education.explore.service.helper.StreamDataHelper;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static com.paytm.digital.education.explore.constants.ExploreConstants.*;
+import static com.paytm.digital.education.explore.constants.ExploreConstants.STREAMS;
+import static com.paytm.digital.education.explore.constants.ExploreConstants.OVERALL_RANKING;
+import static com.paytm.digital.education.explore.constants.ExploreConstants.SIX;
+import static com.paytm.digital.education.explore.constants.ExploreConstants.YEARS;
+import static com.paytm.digital.education.explore.constants.ExploreConstants.BLANK;
 
 @Service
 @AllArgsConstructor
@@ -22,7 +26,7 @@ public class DealsServiceImpl {
 
     public List<String> getSelectItems(String type) {
         List<String> itemsList = new ArrayList<>();
-        if (Objects.nonNull(type)) {
+        if (!StringUtils.isEmpty(type)) {
             if (type.equalsIgnoreCase(STREAMS)) {
                 Set<String> streams = streamDataHelper.getStreamLabelMap().entrySet().stream()
                         .map(e -> e.getValue()).filter(e -> !e.equalsIgnoreCase(OVERALL_RANKING))
