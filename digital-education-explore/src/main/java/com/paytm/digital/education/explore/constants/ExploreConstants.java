3b5f243da09e86ea29e7bac4247715d19c463092
Arjun <arjun.prajapati@paytm.com>
2019-08-29 10:22:55 +0000

    Merged in EDU-2737 (pull request #607)
    EDU-2737 logo prefix changes for school
    
    Approved-by: Karan Verma <karan2.verma@paytm.com>

--- digital-education-explore/src/main/java/com/paytm/digital/education/explore/constants/ExploreConstants.java
+++ digital-education-explore/src/main/java/com/paytm/digital/education/explore/constants/ExploreConstants.java
@@ -259,6 +259,7 @@ public interface ExploreConstants {
 
     String COLLEGE_FOCUS   = "colleges_focus";
     String TOP_COLLEGES    = "top_colleges";
+    String TOP_SCHOOLS    = "top_schools";
     String TOP_EXAMS       = "top_exams";
     String TOP_EXAMS_APP   = "top_exams_app";
     String ICON            = "icon";
