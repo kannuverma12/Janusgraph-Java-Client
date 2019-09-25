815606d41c3260093f721a510ff447d7c5a026ee
Karan Verma <karan2.verma@paytm.com>
2019-09-25 06:52:48 +0000

    Merged in EDU-3506 (pull request #747)
    EDU-3506 admin Bug resolution
    
    Approved-by: Arjun <arjun.prajapati@paytm.com>
    Approved-by: Shallu Mangla <shallu.mangla@paytmmall.com>
    Approved-by: Karan Verma <karan2.verma@paytm.com>

--- digital-education-explore/src/main/java/com/paytm/digital/education/admin/controller/CampusAdminController.java
+++ digital-education-explore/src/main/java/com/paytm/digital/education/admin/controller/CampusAdminController.java
@@ -1,6 +1,8 @@
 package com.paytm.digital.education.admin.controller;
 
-
+import com.paytm.digital.education.admin.request.AmbassadorRequest;
+import com.paytm.digital.education.admin.request.ArticleRequest;
+import com.paytm.digital.education.admin.request.EventRequest;
 import com.paytm.digital.education.admin.response.CampusAdminResponse;
 import com.paytm.digital.education.admin.service.CampusAdminService;
 import com.paytm.digital.education.explore.xcel.model.XcelArticle;
@@ -8,6 +10,7 @@ import com.paytm.digital.education.explore.xcel.model.XcelCampusAmbassador;
 import com.paytm.digital.education.explore.xcel.model.XcelEvent;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 import static com.paytm.digital.education.explore.constants.ExploreConstants.EDUCATION_BASE_URL;
@@ -23,13 +27,14 @@ import static com.paytm.digital.education.explore.constants.ExploreConstants.EDU
 @RestController
 @RequestMapping(EDUCATION_BASE_URL)
 @AllArgsConstructor
+@Validated
 public class CampusAdminController {
 
     private CampusAdminService campusAdminService;
 
     @PutMapping("/admin/v1/campus-ambassadors")
     public @ResponseBody CampusAdminResponse addAmbassadors(@RequestBody
-            XcelCampusAmbassador campusAmbassador) {
+            @Valid AmbassadorRequest campusAmbassador) {
         log.info("Adding campus ambassador.");
         return campusAdminService.addAmbassadors(campusAmbassador);
     }
@@ -42,7 +47,7 @@ public class CampusAdminController {
 
     @PutMapping("/admin/v1/campus-articles")
     public @ResponseBody CampusAdminResponse addArticles(@RequestBody
-            XcelArticle campusArticle) {
+            @Valid ArticleRequest campusArticle) {
         log.info("Adding campus article.");
         return campusAdminService.addArticles(campusArticle);
     }
@@ -55,7 +60,7 @@ public class CampusAdminController {
 
     @PutMapping("/admin/v1/campus-events")
     public @ResponseBody CampusAdminResponse addEvent(@RequestBody
-            XcelEvent campusEvent) {
+            @Valid EventRequest campusEvent) {
         log.info("Adding campus event.");
         return campusAdminService.addEvents(campusEvent);
     }
