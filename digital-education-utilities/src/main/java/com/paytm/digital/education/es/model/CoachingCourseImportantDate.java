53f8e558a1911328604ff396b017295ee0bbdf00
Manoj Kumar <manoj27.kumar@paytm.com>
2019-10-17 07:32:15 +0000

    Merged in EDU-3818 (pull request #858)
    EDU-3818
    
    * added no args constructor
    
    * updated the version
    
    Approved-by: Divyanshu <divyanshu.sharma@paytmmall.com>

--- digital-education-utilities/src/main/java/com/paytm/digital/education/es/model/CoachingCourseImportantDate.java
+++ digital-education-utilities/src/main/java/com/paytm/digital/education/es/model/CoachingCourseImportantDate.java
@@ -6,9 +6,11 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
