145b196ee9733749c4f251df258a0b4bbe1c8938
Yasser Hussain <yasser.hussain@paytmmall.com>
2019-11-05 15:47:05 +0530

    more code

--- digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/impl/RedisService.java
+++ digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/impl/RedisService.java
@@ -15,24 +15,38 @@ public class RedisService {
     private final RedisTemplate< String, Object> template;
 
     public Object get(String key, ProceedingJoinPoint joinPoint) {
-        String data = (String) template.opsForValue().get(key);
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        if (data == null) {
-            return generateNewDataAndCache(key, methodSignature.getReturnType(), null, joinPoint);
-        }
+        boolean shouldRetry = false;
+        do {
+            String data = (String) template.opsForValue().get(key);
+            MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+            if (data == null) {
+                try {
+                    return generateNewDataAndCache(key, methodSignature.getReturnType(), null, joinPoint);
+                } catch (KeyLockedException e) {
+                    Thread.sleep(500L);
+                    shouldRetry = true;
+                } catch (InterruptedException e) {
 
-        String[] parts = data.split("]\\*\\*", 2);
-        String dateTimeString = parts[0].substring(3);
-        DateTime date = DateTime.parse(dateTimeString);
+                }
+            }
 
-        if (date.isAfterNow()) {
-            return JsonUtils.fromJson(parts[1], methodSignature.getReturnType());
-        }
+            String[] parts = data.split("]\\*\\*", 2);
+            String dateTimeString = parts[0].substring(3);
+            DateTime date = DateTime.parse(dateTimeString);
 
-        return generateNewDataAndCache(key, methodSignature.getReturnType(), parts[1], joinPoint);
+            if (date.isAfterNow()) {
+                return JsonUtils.fromJson(parts[1], methodSignature.getReturnType());
+            }
+
+            try {
+                return generateNewDataAndCache(key, methodSignature.getReturnType(), null, joinPoint);
+            } catch (KeyLockedException e) {
+                shouldRetry = true;
+            }
+        } while (shouldRetry);
     }
 
-    private Object generateNewDataAndCache(String key, Class clazz, String oldData, ProceedingJoinPoint joinPoint) {
+    private Object generateNewDataAndCache(String key, Class clazz, String oldData, ProceedingJoinPoint joinPoint) throws KeyLockedException{
         try {
             Object o = joinPoint.proceed();
             int millis = 5 * 60 * 1000;
@@ -44,4 +58,6 @@ public class RedisService {
             return JsonUtils.fromJson(oldData, clazz);
         }
     }
+
+    private static class KeyLockedException extends RuntimeException {}
 }
