468b45bde98d4e363d2f9e65399f825350d325fd
Vivek Gupta <vivek4.gupta@paytmmall.com>
2019-11-11 18:46:27 +0530

    adding refactorings

--- digital-education-application/src/main/java/com/paytm/digital/education/application/config/metric/DatadogFilter.java
+++ digital-education-application/src/main/java/com/paytm/digital/education/application/config/metric/DatadogFilter.java
@@ -2,26 +2,17 @@ package com.paytm.digital.education.application.config.metric;
 
 import com.paytm.education.logger.Logger;
 import com.paytm.education.logger.LoggerFactory;
-
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.core.config.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RequestCallback;
-import org.springframework.web.client.ResponseExtractor;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.UrlPathHelper;
 
-import java.io.IOException;
-import java.net.URI;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 @Order(1)
@@ -33,12 +24,13 @@ public class DatadogFilter extends OncePerRequestFilter {
     private MetricsAgent metricsAgent;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain)
             throws ServletException, IOException {
 
 
         long startTime = System.currentTimeMillis();
-        try{
+        try {
             filterChain.doFilter(request, response);
         } finally {
             long elapsed = System.currentTimeMillis() - startTime;
@@ -54,16 +46,16 @@ public class DatadogFilter extends OncePerRequestFilter {
         }
     }
 
-    private String getURIStructure(String resourcePath){
+    private String getURIStructure(String resourcePath) {
         StringBuilder metricsURI = new StringBuilder();
-        String numRegex   = ".*[0-9].*";
+        String numRegex = ".*[0-9].*";
         String[] pattern = resourcePath.split("/");
-        for (String part : pattern){
-            if (!part.isEmpty()){
+        for (String part : pattern) {
+            if (!part.isEmpty()) {
                 metricsURI.append("/");
                 if (part.matches(numRegex) && !part.matches("v\\d+")) {
                     metricsURI.append("XXXXX");
-                }else{
+                } else {
                     metricsURI.append(part);
                 }
             }
