081419895636b21fd250ea25cd87b4524db22b5a
Shivam Singhal <shivam.singhal@paytm.com>
2019-11-14 12:16:26 +0530

    Added validated to purchase controller

--- digital-education-coaching/src/main/java/com/paytm/digital/education/coaching/consumer/controller/PurchaseController.java
+++ digital-education-coaching/src/main/java/com/paytm/digital/education/coaching/consumer/controller/PurchaseController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import static com.paytm.digital.education.coaching.constants.CoachingConstants.U
 @Slf4j
 @RestController
 @RequestMapping(value = COACHING_BASE + V1)
+@Validated
 public class PurchaseController {
 
     @Autowired private PurchaseService purchaseService;
