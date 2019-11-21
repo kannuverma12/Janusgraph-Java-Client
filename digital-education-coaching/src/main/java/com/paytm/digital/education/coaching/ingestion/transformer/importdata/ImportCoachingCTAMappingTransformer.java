82639e5248565cf648e8d94325e55b7d8d48c27f
Vivek Gupta <vivek4.gupta@paytmmall.com>
2019-11-21 14:24:24 +0530

    adding non null value in map

--- digital-education-coaching/src/main/java/com/paytm/digital/education/coaching/ingestion/transformer/importdata/ImportCoachingCTAMappingTransformer.java
+++ digital-education-coaching/src/main/java/com/paytm/digital/education/coaching/ingestion/transformer/importdata/ImportCoachingCTAMappingTransformer.java
@@ -7,6 +7,7 @@ import com.paytm.digital.education.enums.CTAViewType;
 import com.paytm.education.logger.Logger;
 import com.paytm.education.logger.LoggerFactory;
 
+import java.util.Map;
 import java.util.Optional;
 
 public class ImportCoachingCTAMappingTransformer {
@@ -18,12 +19,16 @@ public class ImportCoachingCTAMappingTransformer {
         if (null == form) {
             return null;
         }
+
+        Map<CTAViewType, Long> ctaInfo = null;
+        CTAViewType ctaViewType = CTAViewType.fromString(form.getViewType());
+        if (ctaViewType != null && form.getCtaId() != null) {
+            ctaInfo = ImmutableMap.of(CTAViewType.fromString(form.getViewType()), form.getCtaId());
+        }
         return CoachingCoursePatchRequest
                 .builder()
                 .courseId(form.getCourseId())
-                .ctaInfo(Optional.ofNullable(CTAViewType.fromString(form.getViewType()))
-                        .map(ctaViewType -> ImmutableMap.of(ctaViewType, form.getCtaId()))
-                        .orElse(ImmutableMap.of()))
+                .ctaInfo(Optional.ofNullable(ctaInfo).orElse(ImmutableMap.of()))
                 .build();
     }
 }
