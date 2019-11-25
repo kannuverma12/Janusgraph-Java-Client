8148e30e6edaa08b144946d0f3b5701337a02f7b
Shivam Singhal <shivam.singhal@paytm.com>
2019-11-25 17:42:24 +0530

    Vault integration temp commit

--- digital-education-application/src/main/java/com/paytm/digital/education/application/Application.java
+++ digital-education-application/src/main/java/com/paytm/digital/education/application/Application.java
@@ -1,6 +1,9 @@
 package com.paytm.digital.education.application;
 
 import com.paytm.digital.education.application.constant.Constant;
+import com.paytm.education.logger.Logger;
+import com.paytm.education.logger.LoggerFactory;
+import com.paytm.vault.VaultUtil;
 import org.apache.logging.log4j.core.lookup.MainMapLookup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,11 +22,19 @@ import paytm.auth.personaaclclient.config.EnableACLPersonaAuth;
 @ComponentScan({Constant.EDUCATION_BASE_PACKAGE, Constant.PERSONA_APPLICATION_PACKAGE})
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     static {
         MainMapLookup.setMainArguments("digital-education-service");
     }
 
     public static void main(String[] args) {
+
+        try {
+            VaultUtil.injectVaultProperties("spring.profiles.active");
+        } catch (Exception e) {
+            log.error("Could not inject vault properties ", e);
+        }
 
         SpringApplication.run(Application.class, args);
     }
