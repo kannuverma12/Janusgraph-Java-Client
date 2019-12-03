95f21457b5407f0fb05c0604cd8160b200cb2ac8
Arjun Prajapati <arjun.prajapati@paytm.com>
2019-12-03 14:21:33 +0530

    EDU-3415-exam collection change

--- digital-education-utilities/src/main/java/com/paytm/digital/education/database/entity/Exam.java
+++ digital-education-utilities/src/main/java/com/paytm/digital/education/database/entity/Exam.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@Document(collection = "exam_new")
+@Document(collection = "exam")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Exam extends Base {
 
