71ded0d7d1235974b4cebb5d3acb7bcc095f3f91
Shivam Singhal <shivam.singhal@paytm.com>
2020-01-06 18:27:13 +0530

    EDU-4615 : vault integration done for staging with profile check

--- digital-education-application/src/main/java/com/paytm/digital/education/application/Application.java
+++ digital-education-application/src/main/java/com/paytm/digital/education/application/Application.java
@@ -7,25 +7,21 @@ import com.paytm.education.logger.Logger;
 import com.paytm.education.logger.LoggerFactory;
 import com.paytm.vault.VaultUtil;
 import org.apache.logging.log4j.core.lookup.MainMapLookup;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.servlet.DispatcherServlet;
 import paytm.auth.personaaclclient.config.EnableACLPersonaAuth;
-
-import java.util.Arrays;
-import java.util.List;
 
 @EnableRetry
 @SpringBootApplication(exclude = {EmbeddedMongoAutoConfiguration.class})
@@ -34,32 +30,46 @@ import java.util.List;
 @EnableMongoAuditing
 @EnableMongoRepositories(basePackages = Constant.EDUCATION_BASE_PACKAGE)
 @ComponentScan({Constant.EDUCATION_BASE_PACKAGE, Constant.PERSONA_APPLICATION_PACKAGE})
-public class Application implements CommandLineRunner {
+public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
-
-    @Autowired
-    private Environment env;
 
     static {
         MainMapLookup.setMainArguments("digital-education-service");
     }
 
-    public static void main(String[] args) throws Exception {
 
-        SpringApplication.run(Application.class, args);
+    static class FetchVault implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+        @Override
+        public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+            Boolean isVaultSource = event.getEnvironment()
+                    .getProperty("vault.source", Boolean.class, Boolean.FALSE);
+            log.info("isVaultSource : {}", isVaultSource);
+            if (isVaultSource) {
+                log.info("Going to load vault properties");
+                try {
+                    loadVaultProperties();
+                } catch (Exception e) {
+                    log.error("Exception while loading vault properties", e);
+                    System.exit(1);
+                }
+            }
+        }
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
-        if (activeProfiles.contains("staging") || activeProfiles.contains("production")) {
-            try {
-                VaultUtil.injectVaultProperties("spring.profiles.active");
-            } catch (Exception e) {
-                log.error("Could not inject vault properties ", e);
-                throw e;
-            }
+    public static void main(String[] args) {
+
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new FetchVault());
+        springApplication.run(args);
+    }
+
+    private static void loadVaultProperties() throws Exception {
+        try {
+            VaultUtil.injectVaultProperties("spring.profiles.active");
+        } catch (Exception e) {
+            log.error("Could not inject vault properties ", e);
+            throw e;
         }
     }
 
