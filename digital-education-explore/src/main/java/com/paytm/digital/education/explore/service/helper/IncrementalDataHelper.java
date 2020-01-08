6e51ce7a6349cd21bcc7fe597be4c5111d148d79
kverma07 <karan2.verma@paytm.com>
2020-01-08 17:44:36 +0530

    EDU-0000 course file count issue

--- digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/helper/IncrementalDataHelper.java
+++ digital-education-explore/src/main/java/com/paytm/digital/education/explore/service/helper/IncrementalDataHelper.java
@@ -126,19 +126,19 @@ public class IncrementalDataHelper {
 
             String instFileName = String.valueOf(versioningData.get(NEXT_INSTITUTE_FILE_VERSION));
             String instFinalName = instFileName.indexOf(".") > 0
-                    ? examFileName.substring(0, examFileName.indexOf(".")) : examFileName;
+                    ? instFileName.substring(0, instFileName.indexOf(".")) : instFileName;
             currentInstituteFileName = MessageFormat.format(SFTP_INSTITUTE_FILE_NAME_FORMAT,
                     instFinalName);
 
             String courseFileName = String.valueOf(versioningData.get(NEXT_COURSE_FILE_VERSION));
             String courseFinalName = courseFileName.indexOf(".") > 0
-                    ? examFileName.substring(0, examFileName.indexOf(".")) : examFileName;
+                    ? courseFileName.substring(0, courseFileName.indexOf(".")) : courseFileName;
             currentCourseFileName = MessageFormat.format(SFTP_COURSE_FILE_NAME_FORMAT,
                     courseFinalName);
 
             String schoolFileName = String.valueOf(versioningData.get(NEXT_SCHOOL_FILE_VERSION));
             String schoolFinalName = schoolFileName.indexOf(".") > 0
-                    ? examFileName.substring(0, examFileName.indexOf(".")) : examFileName;
+                    ? schoolFileName.substring(0, schoolFileName.indexOf(".")) : schoolFileName;
             currentSchoolFileName = MessageFormat.format(SFTP_SCHOOL_FILE_NAME_FORMAT,
                     schoolFinalName);
         }
